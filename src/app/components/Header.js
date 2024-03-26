ed39aacf2a95518bb1362e1f72a64d9dd0c8cbf7
AnuragDobhal <dobhalannu246@gmail.com>
2024-03-26 15:00:05 +0530

    logo update

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
         <header className={styles.main_header}>
             <div className={styles.navbar_brand}>
                 <Link href="/">
-                    <Image src="/logowwbg.png" alt="my logo image" width={150} height={40}/>
+                    <Image src="/redlogo.png" alt="my logo image" width={150} height={40}/>
                 </Link>
             </div>
             <Nav/>
