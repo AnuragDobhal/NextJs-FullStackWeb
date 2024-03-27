57ec05fe0e1d5f3e90f4cc5ea3fef19d0a999d90
AnuragDobhal <dobhalannu246@gmail.com>
2024-03-27 12:12:54 +0530

    some major updates and added a services Page/Route

---
+++ src/app/components/ServicesHero.js
@@ -0,0 +1,92 @@
+"use client";
+// import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "@/app/styles/servicehero.module.css";
+const ServiceHero = () => {
+  return (
+    <main className={styles.wrapper}>
+      <h2 className={styles.heading}>Explore</h2>
+      <section className={styles.hero}>
+        <h1>Unlock Tailored Solutions</h1>
+        <article>
+          <p>
+          "Unlock unparalleled solutions for your business challenges. Reach out to us today and elevate your business to new heights."
+          </p>
+          <Link className={styles.visits} href="/movie">
+            Explore Movies
+          </Link>
+        </article>
+      </section>
+      <section className={styles.breweries} id="breweries">
+        <ul>
+          <li>
+            <figure>
+              <Image
+                src="/about.jpg"
+                alt="Several hands holding beer glasses"
+                width={500}
+                height={500}
+              />
+              <figcaption>
+                <h3>Billions upon billions</h3>
+              </figcaption>
+            </figure>
+            <p>
+              Made in the interiors of collapsing stars star stuff harvesting
+              star light venture billions upon billions Drake Equation brain is
+              the seed of intelligence?
+            </p>
+            <Link className={styles.visit} href="#">
+              View Article
+            </Link>
+          </li>
+          <li>
+            <figure>
+              <Image
+                src="/banner.jpg"
+                alt="Several friends doing a toast"
+                width={500}
+                height={500}
+              />
+              <figcaption>
+                <h3>Drake Equation</h3>
+              </figcaption>
+            </figure>
+            <p>
+              Another world citizens of distant epochs from which we spring
+              descended from astronomers Orion's sword shores of the cosmic
+              ocean.
+            </p>
+            <Link className={styles.visit} href="#">
+              View Article
+            </Link>
+          </li>
+          <li>
+            <figure>
+              <Image
+                src="/about.jpg"
+                alt="Three different glasses of beer"
+                width={500}
+                height={500}
+              />
+              <figcaption>
+                <h3>Vast cosmic arena</h3>
+              </figcaption>
+            </figure>
+            <p>
+              Galaxies the ash of stellar alchemy prime number science
+              inconspicuous motes of rock and gas brain is the seed of
+              intelligence.
+            </p>
+            <Link className={styles.visit} href="#">
+              View Article
+            </Link>
+          </li>
+        </ul>
+      </section>
+    </main>
+  );
+};
+
+export default ServiceHero;
--- src/app/services/page.js
+++ src/app/services/page.js
@@ -2,13 +2,16 @@ import Herosection from "@/app/components/Herosection";
 // import OurClients from "@/app/components/OurClients";
 // //import AboutUs from "@app/components/About"
 // // import AboutUs from "../components/About";
+
 import React from 'react'
+import ServiceHero from "../components/ServicesHero";
 
 function Services() {
   return (
     <div>
-  <Herosection title={"OUR Services"} imageUrl ={"/about.svg"} />
+  {/* <Herosection title={"OUR Services"} imageUrl ={"/about.svg"} /> */}
 
+<ServiceHero/>
         {/* <AboutUs/>
         <OurClients/> */}
     </div>
