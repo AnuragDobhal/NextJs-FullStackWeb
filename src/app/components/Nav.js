17043ce7d98459e91299fb11734152c7428bfc1c
AnuragDobhal <dobhalannu246@gmail.com>
2024-03-31 13:01:33 +0530

    changes in navbar

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -37,16 +37,7 @@ const Nav = () => {
                                   onClick={() => setOpenMenu(false)}
                             >Home</Link>
                         </li>
-                        <li className={styles.navbarItem}>
-                            <Link className={styles.navbarLink} href="/about"
-                                  onClick={() => setOpenMenu(false)}
-                            >About</Link>
-                        </li>
-                        <li className={styles.navbarItem}>
-                            <Link className={styles.navbarLink}
-                                  onClick={() => setOpenMenu(false)}
-                                  href="/movie">Movie</Link>
-                        </li>
+                      
                         <li className={styles.navbarItem}>
                             <Link className={styles.navbarLink}
                                   onClick={() => setOpenMenu(false)}
@@ -55,7 +46,23 @@ const Nav = () => {
                         <li className={styles.navbarItem}>
                             <Link className={styles.navbarLink}
                                   onClick={() => setOpenMenu(false)}
+                                  href="/movie">Movie</Link>
+                        </li>
+                        <li className={styles.navbarItem}>
+                            <Link className={styles.navbarLink} href="/about"
+                                  onClick={() => setOpenMenu(false)}
+                            >About</Link>
+                        </li>
+                     
+                        <li className={styles.navbarItem}>
+                            <Link className={styles.navbarLink}
+                                  onClick={() => setOpenMenu(false)}
                                   href="/services">Services</Link>
+                        </li>
+                        <li className={styles.navbarItem}>
+                            <Link className={styles.navbarLink}
+                                  onClick={() => setOpenMenu(false)}
+                                  href="/covid-19">Covid Cases</Link>
                         </li>
                     </ul>
 
