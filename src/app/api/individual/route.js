095c2aec81d94c00a841fa6b251d2267c104f397
AnuragDobhal <dobhalannu246@gmail.com>
2024-04-08 18:21:09 +0530

    removed post api induvidual code

--- src/app/api/individual/route.js
+++ src/app/api/individual/route.js
@@ -6,13 +6,3 @@ export function GET(){
     const data = individuals;
     return NextResponse.json(data,{status:200});
 }
-
-
-// export async function POST(request){
-//     let payload= await request.json
-//      console.log(payload.name)
-// if(!payload.name||!payload.age||!payload.email||!payload.company||!payload.jobRole||!payload.id){
-// return NextResponse.json({result:"required fields not found",success:false})
-// }
-//     return NextResponse.json({result:"hello"});
-// }
